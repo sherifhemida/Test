cc77849456ec2d8e94e0322768e2578929fbd2e9
Sherif Hemida <sherifhemida@outlook.com>
2023-03-26 14:17:02 -0400

    open ai NLP to SQL

--- test.py
+++
@@ -1,7 +0,0 @@
-#list current subdirectories excluding subdirectories of current subdirectories
-import os
-for root, dirs, files in os.walk(".", topdown=True):
-    dirs[:] = [d for d in dirs if d not in ['subdir1', 'subdir2']]
-    print(root)
-    for name in files:
-        print(os.path.join(root, name))
