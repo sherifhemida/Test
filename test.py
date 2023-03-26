56ade93a6f1a1fbd0b43b97beeb7ef558754d2f3
Sherif Hemida <sherifhemida@outlook.com>
2023-03-26 13:35:09 -0400

    sdfsdf sdfsdfsd

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-#list subfolder names in the current directory
+#list current subdirectories excluding subdirectories of current subdirectories
 import os
-for name in os.listdir('.'):
-    if os.path.isdir(name):
-        print(name)
-
-#print date time
-import datetime
-print (datetime.datetime.now())
+for root, dirs, files in os.walk(".", topdown=True):
+    dirs[:] = [d for d in dirs if d not in ['subdir1', 'subdir2']]
+    print(root)
+    for name in files:
+        print(os.path.join(root, name))
